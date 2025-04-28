f972d7e3ae663456d06d47861a51e420eabbb2e0
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-04-28 20:17:40 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Lifeprojects.de">
+//     Class: Program
+//     Copyright © Lifeprojects.de 2025
+// </copyright>
+// <Template>
+// 	Version 2.0.2025.0, 28.4.2025
+// </Template>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>28.04.2025 20:15:48</date>
+//
+// <summary>
+// Konsolen Applikation mit Menü
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace Console.FluentInterfaceDesign
+{
+    /* Imports from NET Framework */
+    using System;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. Menüpunkt 1");
+                Console.WriteLine("2. Menüpunkt 2");
+                Console.WriteLine("X. Beenden");
+                Console.WriteLine("Wählen Sie einen Menüpunkt oder 'x' für beenden");
+                ConsoleKey key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.X)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    if (key == ConsoleKey.D1)
+                    {
+                        MenuPoint1();
+                    }
+                    else if (key == ConsoleKey.D2)
+                    {
+                        MenuPoint2();
+                    }
+                }
+            }
+            while (true);
+        }
+
+        private static void MenuPoint1()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
+            Console.ReadKey();
+        }
+
+        private static void MenuPoint2()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
+            Console.ReadKey();
+        }
+    }
+}
